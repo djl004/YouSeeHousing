056fed2293c6dcbe4fe95f596447e0746b0be515
qownsgml93 <jub007@ucsd.edu>
2019-04-20 19:57:31 -0700

    Working on basic UI

--- app/src/main/java/com/example/youseehousing/MainActivity.java
+++ app/src/main/java/com/example/youseehousing/MainActivity.java
@@ -20,22 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         Signup = findViewById(R.id.signup);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
+        // When "SIGN UP" button is clicked.
         Signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
