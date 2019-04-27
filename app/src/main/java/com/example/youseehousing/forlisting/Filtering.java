149eef8cb1fb6a14f7940d72d837f1816bf0a220
aam0322 <ma.aaron.ma@gmail.com>
2019-04-27 14:16:47 -0700

    Filter and Sort classes. I decided to split them up into different files, because I think they might become a little bit long.

---
+++ app/src/main/java/com/example/youseehousing/forlisting/Filtering.java
@@ -0,0 +1,45 @@
+package com.example.youseehousing.forlisting;
+
+
+import java.util.ArrayList;         // Want to use ArrayList
+
+
+
+
+
+public class Filtering{
+
+
+
+
+
+
+    // This static method is called to sort the ArrayList of Listing objects
+    public static ArrayList<Listing> filter(){
+
+
+        return null;
+    }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/example/youseehousing/forlisting/Sorting.java
@@ -0,0 +1,28 @@
+package com.example.youseehousing.forlisting;
+
+
+
+import java.util.ArrayList;         // Want to use ArrayList
+
+
+
+
+public class Sorting{
+
+
+
+
+    // This static method is called to sort the ArrayList of Listing objects
+    public static ArrayList <Listing> sort(){
+
+
+        return null;
+    }
+
+
+
+
+
+
+
+}
