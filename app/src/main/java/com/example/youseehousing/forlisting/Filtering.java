b2c1f28e143c1c3af56bfcebd7db98f4ad15ee50
aam0322 <ma.aaron.ma@gmail.com>
2019-04-29 14:01:37 -0700

    Basic outline for the filter() method set up.

--- app/src/main/java/com/example/youseehousing/forlisting/Filtering.java
+++ app/src/main/java/com/example/youseehousing/forlisting/Filtering.java
@@ -7,19 +7,47 @@ import java.util.ArrayList;         // Want to use ArrayList
 
 
 
+
+
+
+
+
+
 public class Filtering{
 
 
+    /**
+     * This static method is used to filter out certain listings
+     * @param l: The ArrayList of Listings to filter out:
+     * @param param: The general criteria to filter. Can be one of the following:
+     *               - "distance" to filter by distance
+     *               - "price" to filter by price
+     *               - "hasUtils" to filter out listings that do not include utilities in rent cost
+     *               - "hasWD" to filter out listings without WD
+     *               - "hasFurniture" to filter out listings without furniture
+     *               - "numBaths" to filter out listings without right number of baths
+     *               - "numRooms" to filter out listings without the right number of rooms
+     *               - "numVacancies" to filter by number of vacancies
+     *               - "hasPets" to filter out listings that don't allow pets
+     *               - "leaseDuration" to filter by lease duraction
+     * @param botLimit and topLimit: Some criteria needs additional detail. See below:
+     *               - distance needs lower bound and upper bound, inclusive
+     *               - price needs lower and upper bound, inclusive
+     *               - numBaths needs lower and upper bound, inclusive
+     *               - numRooms needs lower rand upper bound, inclusive
+     *               - numVacancies needs lower and upper bound, inclusive
+     *               - leasDuration needs lowe rnad upper bound, inclusive
+     * @return: The filtered list
+     */
+    public static ArrayList<Listing> filter(ArrayList<Listing> l, String param, int botLimit, int topLimit){
 
 
 
 
-    // This static method is called to sort the ArrayList of Listing objects
-    public static ArrayList<Listing> filter(){
 
-
-        return null;
-    }
+        // The list is filtered, so return it
+        return l;
+    } // end of filer()
 
 
 
@@ -27,7 +55,7 @@ public class Filtering{
 
 
 
-}
+} // end of public class Filtering
 
 
 
