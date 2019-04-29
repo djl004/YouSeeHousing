39eb92fac11966e187329a63f912333f2c15c7b5
aam0322 <ma.aaron.ma@gmail.com>
2019-04-29 14:41:58 -0700

    Filtering tentatively finished.

--- app/src/main/java/com/example/youseehousing/forlisting/Filtering.java
+++ app/src/main/java/com/example/youseehousing/forlisting/Filtering.java
@@ -37,9 +37,97 @@ public class Filtering{
      *               - numRooms needs lower rand upper bound, inclusive
      *               - numVacancies needs lower and upper bound, inclusive
      *               - leasDuration needs lowe rnad upper bound, inclusive
+     *               Note: If the param doesn't use the botLimit or topLimit, pass in whatever.
      * @return: The filtered list
      */
     public static ArrayList<Listing> filter(ArrayList<Listing> l, String param, int botLimit, int topLimit){
+
+
+        // Check if this particular method call is using limits or not
+        boolean usesLimits = false;
+        if(param == "distance" || param == "price" || param == "numBaths" || param == "numRooms"
+                || param == "numVacancies" || param == "leaseDuration"){
+            usesLimits = true;
+        }
+
+
+        // Filter using a parameter that uses limits
+        if(usesLimits){
+
+        }
+            // Go through the ArrayList and delete Listings that don't fit
+            for(int i = 0; i < l.size(); i++) {
+
+                Listing currentObj = l.get(i);
+
+
+                // For params that include a botLimit and a topLimit, get the relevant instance variable
+                int relevantVar = 0;
+                if (param == "distance") {
+                    relevantVar = currentObj.getDistance();
+                } else if (param == "price") {
+                    relevantVar = currentObj.getPrice();
+                } else if (param == "numBaths") {
+                    relevantVar = currentObj.getNumBaths();
+                } else if (param == "numRooms") {
+                    relevantVar = currentObj.getNumRooms();
+                } else if (param == "numVacancies") {
+                    relevantVar = currentObj.getNumVacancies();
+                } else if (param == "hasPets") {
+                    relevantVar = currentObj.getHasPets();
+                } else if (param == "leaseDuration") {
+                    relevantVar = currentObj.getLeaseDuration();
+                }
+
+
+                // If the current Listing don't fit in the limits, remove that Listing
+                if(relevantVar < botLimit || relevantVar > topLimit) {
+                    l.remove(i);           // Remove the element
+                    i--;                   // Decrement i to account for the removal of one element
+                }
+
+
+            } // End of going through every element in the list
+        } // end of filtering by parameters that use limits
+
+
+
+
+
+        // Filter using a parameter that doesn't use limits
+        else{
+
+            // Go through the ArrayList and delete Listings that don't fit
+            for(int i = 0; i < l.size(); i++) {
+
+
+                Listing currentObj = l.get(i);
+
+                // For params that don't include a botLimit and a topLimit, get the relevant boolean
+                boolean relevantVar = false;
+                if (param == "hasUtils") {
+                    relevantBool = currentObj.isHasUtils;
+                } else if (param == "hasWD") {
+                    relevantBool = currentObj.isHasWD;
+                } else if (param == "hasFurniture") {
+                    relevantBool = currentObj.isHasFurniture;
+                } else if (param == "hasPets") {
+                    relevantBool = currentObj.isHasPets;
+                }
+
+
+                // For the current Listing, remove it if it doesn't qualify
+                if(relevantVar == false){
+                    l.remove(i);           // Remove the element
+                    i--;                   // Decrement i to account for the removal of one element
+                }
+
+
+
+            } // End of going through every element in the list
+        } // end of filtering by parameters that don't use limits
+
+
 
 
 
@@ -47,7 +135,7 @@ public class Filtering{
 
         // The list is filtered, so return it
         return l;
-    } // end of filer()
+    } // end of filter()
 
 
 
