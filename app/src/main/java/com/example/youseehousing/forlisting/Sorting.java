2ada56a69c5a237aaeec3d1f8ab3097198185cfd
rubenren <jr.10101@gmail.com>
2019-04-29 17:25:28 -0700

    reverted changes that broke build

--- app/src/main/java/com/example/youseehousing/forlisting/Sorting.java
+++ app/src/main/java/com/example/youseehousing/forlisting/Sorting.java
@@ -3,85 +3,26 @@ package com.example.youseehousing.forlisting;
 
 
 import java.util.ArrayList;         // Want to use ArrayList
-import java.util.Collection;        // Use Collection to sort, to reverse a list
 
 
 
-/**
- * Contains the static method for sorting.
- */
+
 public class Sorting{
 
 
-    /**
-     * This static method is called to sort a list of listings
-     * @param l: The arraylist to sort
-     * @param param: The listing parameter to sort by. So far, can be:
-     *               - distance, pass in "distance"
-     *               - price, pass in "price"
-     *               - number of vacancies, pass in "numVacancies"
-     * @param reverse: False to sort from low->high. True to sort from high->low
-     * @return: The sorted list
-     */
-    public static ArrayList <Listing> sort(ArrayList<Listing> l, String param, boolean reverse){
-
-        // Sort according to the parameter passed in
-        if(param.equals("distance")){
-            Collections.sort(l, new SortDistance());
-        } else if (param.equals("price")) {
-            Collections.sort(l, new SortRent());
-        } else if (param.equals("numVacancies")){
-            Collections.sort(l, new SortVacancies());
-        }
 
 
-        // If reverse is True, then reverse the list
-        if(reverse){
-            Collections.reverse(l);
-        }
+    // This static method is called to sort the ArrayList of Listing objects
+    public static ArrayList <Listing> sort(){
 
 
-        // List is sorted, return it
-        return l;
+        return null;
     }
 
-} // end of public class Sorting
-
-
-/**
- * Comparator class to sort by distance
- */
-public class SortDistance implements Comparator<Listing>{
-
-    public int compare(Listing a, Listing b){
-        return a.getDistance() - b.getDistance();
-    }
-
-} // end of public class SortDistance
-
-
-/**
- * Comparator class to sort by rent
- */
-public class SortRent implements Comparator<Listing>{
-
-    public int compare(Listing a, Listing b){
-        return a.getRent() - b.getRent();
-    }
-
-} // end of public class SortRent
-
-
-/**
- * Comparator class to sort by number of vacancies
- */
-public class SortVacancies implements Comparator<Listing>{
-
-    public int compare(Listing a, Listing b){
-        return a.getNumVacancies() - b.getNumVacancies();
-    }
-
-} // end of public class SortVacancies
 
 
 
+
+
+
+}
