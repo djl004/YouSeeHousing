c0c2ece2be64de18384d0c88addf5ba37f924f55
Giovanni V <gvindiol@ucsd.edu>
2019-05-01 03:54:18 -0700

    Added UserPreferences GUI

---
+++ app/src/main/java/com/example/youseehousing/UserPreferences.java
@@ -0,0 +1,13 @@
+package com.example.youseehousing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserPreferences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_preferences);
+    }
+}
