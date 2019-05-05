2d5de2f2ce2db425a8b2c589195d8c627113a78d
aam0322 <ma.aaron.ma@gmail.com>
2019-05-05 11:39:55 -0700

    dBetweenCoords() and setCoords() implemented.

--- app/src/main/java/com/example/youseehousing/forlisting/SetDistance.java
+++ app/src/main/java/com/example/youseehousing/forlisting/SetDistance.java
@@ -12,10 +12,15 @@ import java.util.ArrayList;
 public class SetDistance {
 
 
-
+    // Important variables:
     // If, for whatever reason, a Listing's address could not be converted to coordinates, use -1.
     public static final double NO_LATITUDE = -1;
     public static final double NO_LONGITUDE = -1;
+
+    // Constants used for calculations
+    public static final double R = 6372.8;             // In kilometers
+    public static final double KM_CONSTANT = 0.621371; // How many miles in a kilometer
+
 
 
 
@@ -53,9 +58,15 @@ public class SetDistance {
         // Go through list, and fill in Listings
         for(int i = 0; i < list.size(); i++){
 
-            // If Listing has not been filled (lat and lng == 0), then fill them in.
-            if(list.get(i).getLat() != 0 && list.get(i).getLng() != 0){
 
+            // Get the current Listing for convenience
+            Listing curr = list.get(i);
+
+            // If Listing has not been filled (lat and lng == 0), then fill them in.
+            if(curr.getLat() == 0 && curr.getLng() == 0){
+                double arr[] = getCoords(curr.getAddress());
+                curr.setLat(arr[0]);         // Latitude located in the 1st index
+                curr.setLng(arr[1]);         // Longitude located in the 2nd index
             }
 
 
@@ -75,7 +86,8 @@ public class SetDistance {
 
 
     /**
-     * Helper function for getting coords from an address
+     * Helper function for getting coords from an address. If the coordinates cannot be found, then
+     * the returned double array will be filled with -1.
      * @return a double array of size 2. First index contains lat, second contains lng
      */
     public static double[] getCoords(String address){
@@ -97,22 +109,38 @@ public class SetDistance {
 
 
 
+
+
     /**
      * Helper function used to calculate the distance (in miles) between two sets of coordinates
+     * Resource: https://rosettacode.org/wiki/Haversine_formula#Java
+     *
      *
      * @return the distance, in miles, between the two sets of coordinates
      */
-    public static double dBetweenCoords(double firstLat, double firstLng, double secondLat,
-                                                                          double secondLng){
+    public static double dBetweenCoords(double lat1, double lng1, double lat2, double lng2){
+
 
         // Calculate this and return;
         double distance = 0;
 
 
+        // Haversine formula to calculate distance
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lng2 - lng1);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        double a = Math.pow(Math.sin(dLat / 2),2) + Math.pow(Math.sin(dLon / 2),2) * Math.cos(lat1) * Math.cos(lat2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        distance = R * c;
 
 
 
-        return distance;
+
+        // Convert answer in kilometers to miles, and return
+        return distance * KM_CONSTANT;
+
     } // end of dBetweenCoords()
 
 
