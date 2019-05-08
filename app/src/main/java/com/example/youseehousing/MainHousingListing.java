e1c02a5fc27632939bd7fb5e35aa858366c3bd58
vxe <>
2019-05-08 16:22:35 -0700

    fixed crash on switch activity by adding activity to AndroidManifest.xml. clicking item works now

--- app/src/main/java/com/example/youseehousing/MainHousingListing.java
+++ app/src/main/java/com/example/youseehousing/MainHousingListing.java
@@ -3,10 +3,12 @@ package com.example.youseehousing;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.youseehousing.dummy.DummyContent.DummyItem;
 
-public class MainHousingListing extends FragmentActivity implements ItemFragment.OnListFragmentInteractionListener {
+public class MainHousingListing extends AppCompatActivity implements ItemFragment.OnListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +20,17 @@ public class MainHousingListing extends FragmentActivity implements ItemFragment
     // TODO: Implement what happens when a listing is selected
     public void onListFragmentInteraction(DummyItem item) {
         // TODO: Switching to an activity from a fragment doesn't work this way
-//        selectListingFxn();
+        selectListingFxn();
     }
     private void selectListingFxn() {
-        Intent intent_f = new Intent(MainHousingListing.this, MainListingPage.class);
-        startActivity(intent_f);
+        try {
+            Intent intent_f = new Intent(getApplicationContext(), MainListingPage.class);
+            startActivity(intent_f);
+        }
+        catch (android.content.ActivityNotFoundException e) {
+            e.printStackTrace();
+            Log.e("exception", "exception: "+e);
+        }
+
     }
 }
