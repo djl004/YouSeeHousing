7913ec6fbefa3bbaadb466df6491ab67a0f3605b
aam0322 <ma.aaron.ma@gmail.com>
2019-05-13 18:29:56 -0700

    Created tests for Filtering.java

---
+++ app/src/test/java/com/example/youseehousing/forlistingTests/FilteringTests.java
@@ -0,0 +1,130 @@
+package com.example.youseehousing.forlistingTests;
+
+import com.example.youseehousing.forlisting.Listing;
+import com.example.youseehousing.forlisting.Filtering;
+
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import static org.junit.Assert.*;
+import java.lang.Math;              // To generate random numbers
+
+
+
+
+
+/**
+ * Contains unit tests for testing Filtering.java
+ */
+public class FilteringTests {
+
+
+    // Relevant test variables
+    int testSize = 1000;              // How many items in the list you want to test
+
+
+
+
+
+    @Test
+    /**
+     * Test that the filter works on a single variable (that uses a double)
+     */
+    public void testSingleVariableDouble() {
+
+
+        // Important variables
+        ArrayList<Listing> testList = new ArrayList<Listing>();    // list to use in testing
+        boolean testPassed = true;                           // boolean to keep track of success
+        HashMap<String, String> filter = new HashMap<>();    // What to filter
+
+        // Add a testSize objects to testList
+        for(int i = 0; i < testSize; i++){
+            testList.add(new Listing("meaninglessAddress"));
+        }
+
+
+        // Fill in the price instance variable with random numbers
+        for(int i = 0; i < testSize; i++){
+            testList.get(i).setPrice(Math.random()* 1000);
+        }
+
+
+
+
+        // Filter out prices outside 500-800 inclusive
+        filter.put("price", "500-800");
+        testList = Filtering.filter(filter, testList);
+
+
+
+        // Go through testList, check that no prices outside 500-800
+        for(Listing l: testList){
+            testPassed = testPassed && l.getPrice() >= 500 && l.getPrice() <= 800;
+        }
+
+
+
+        // Check that the test passed
+        assert(testPassed);
+
+    } // end of unit test
+
+
+
+
+
+
+
+
+    @Test
+    /**
+     * Test that the filter works on a single variable (that uses boolean)
+     */
+    public void testSingleVariableBoolean() {
+
+
+        // Important variables
+        ArrayList<Listing> testList = new ArrayList<Listing>();    // list to use in testing
+        boolean testPassed = true;                           // boolean to keep track of success
+        HashMap<String, String> filter = new HashMap<>();    // What to filter
+
+        // Add a testSize objects to testList
+        for(int i = 0; i < testSize; i++){
+            testList.add(new Listing("meaninglessAddress"));
+        }
+
+
+        // Fill in the price instance variable with random numbers
+        for(int i = 0; i < testSize; i++){
+
+            testList.get(i).setHasFurniture(Math.random() < 0.5);
+        }
+
+
+
+
+        // Filter out Listings without furniture
+        filter.put("hasFurniture", "true");
+        testList = Filtering.filter(filter, testList);
+
+
+
+        // Go through testList, check Listing has furniture
+        for(Listing l: testList){
+            testPassed = testPassed && l.isHasFurniture();
+        }
+
+
+
+        // Check that the test passed
+        assert(testPassed);
+
+    } // end of unit test
+
+
+
+
+
+
+} // end of public class SortingUnitTests
