59ec1c4c6781ed1d42f77ff71a6e080e9c79c160
aam0322 <ma.aaron.ma@gmail.com>
2019-05-25 16:09:42 -0700

    Added new test file.

---
+++ app/src/test/java/com/example/youseehousing/ProfileTests/AccountCreationTests.java
@@ -0,0 +1,40 @@
+package com.example.youseehousing.ProfileTests;
+
+
+
+
+public class AccountCreationTests {
+
+/**
+ * NOTE: Writing unit tests seems kind of difficult. So, I'll do it manually.
+ *
+ * 
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+}
