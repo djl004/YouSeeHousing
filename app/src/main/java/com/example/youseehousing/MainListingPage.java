92e07c7266e5803b481ef9b240433900f6f036cc
John B <jub007@ucsd.edu>
2019-05-25 12:55:12 -0700

    Map feature added. However, I still need to resolve the internet connection issue, which causes the failure to convert address to coordinates.

--- app/src/main/java/com/example/youseehousing/MainListingPage.java
+++ app/src/main/java/com/example/youseehousing/MainListingPage.java
@@ -1,12 +1,32 @@
 package com.example.youseehousing;
 
+import android.graphics.BitmapFactory;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.Point;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import com.squareup.picasso.Picasso;
+
+import java.util.List;
+import java.util.Locale;
 
 /**
  * This is the page that has a detailed description of a listing.
@@ -25,16 +45,20 @@ import com.squareup.picasso.Picasso;
  */
 public class MainListingPage extends AppCompatActivity {
 
+    private static final String TAG = "MainListingPage";
     private ImageView imagesView;
     private TextView addressView;
     private TextView captionView;
     private TextView descriptionView;
     private ListingDetails listing;
 
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private double longitude, latitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_listing);
 
         imagesView = (ImageView) findViewById(R.id.image);
         addressView = (TextView) findViewById(R.id.title);
@@ -50,7 +74,7 @@ public class MainListingPage extends AppCompatActivity {
         try {
             String image_0 = listing.getPictures().get(0);
             if(image_0.length() > 0) {
-                Picasso.get().load(listing.getPictures().get(0)).into(imagesView);
+            //    Picasso.get().load(listing.getPictures().get(0)).into(imagesView);   // This throws IllegalArgumentException !!!
             }
             addressView.setText(listing.getAddress());
             captionView.setText(listing.getBath() + " " + listing.getBed());
@@ -62,7 +86,84 @@ public class MainListingPage extends AppCompatActivity {
             Log.e("exception", "exception: no parcel was passed from previous activity!"
                                             + e1);
         }
-    }
 
 
-}
+        /************************************* Map Feature ****************************************/
+
+        Mapbox.getInstance(this,
+                "pk.eyJ1IjoicW93bnNnbWw5MyIsImEiOiJjanZ5emNrMmwwYWo5NDh0cm56bnpsNG1pIn0.JxZmBYwkXwMLb2w7ZdjQIQ");
+        setContentView(R.layout.activity_listing);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        String userAddress = listing.getAddress();
+        Log.d(TAG, "User address: " + userAddress);
+
+        /*
+        tv_check_connection = findViewById(R.id.tv_check_connection);
+        mNetworkReceiver = new NetworkChangeReceiver();
+        registerNetworkBroadcastForNougat();
+*/
+        // Convert user address to coordinates (latitude & longitude)
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        List<Address> addresses;
+
+
+        try {
+            addresses = geocoder.getFromLocationName(userAddress, 1);   // This is causing the internet connection issue
+            Address address = addresses.get(0);
+            longitude = address.getLongitude();
+            latitude = address.getLatitude();
+        } catch (Exception e) {
+            Log.e(TAG, "Exception thrown while converting address to coordinates: " + e.toString());
+        }
+
+
+        // Create the map
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxArg) {
+                MainListingPage.this.mapboxMap = mapboxArg;
+
+                // Setting camera to the user's address
+                CameraPosition position = new CameraPosition.Builder()
+                        .target(new LatLng(latitude, longitude)).build();
+                try {
+                    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 100);
+                    Log.d(TAG, "Camera position changed to (lat: " + latitude + ", lng: " + longitude + ")");
+                } catch (Exception e) {
+                    Log.e(TAG, "Exception thrown while changing camera position: " + e.toString());
+                }
+
+                // Setting the map style
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                        // Map is set up and the style has loaded. Now you can add data or make other map adjustments
+                        // Adding a pin to map
+                        style.addImage("marker-icon-id",
+                                BitmapFactory.decodeResource(
+                                        MainListingPage.this.getResources(), R.drawable.mapbox_marker_icon_default));
+
+                        Log.d(TAG, "pin set at (lat: " + latitude + ", lng: " + longitude + ")");
+
+                        GeoJsonSource geoJsonSource = new GeoJsonSource("source-id", Feature.fromGeometry(
+                                Point.fromLngLat(longitude, latitude)));
+                        style.addSource(geoJsonSource);
+
+                        SymbolLayer symbolLayer = new SymbolLayer("layer-id", "source-id");
+                        symbolLayer.withProperties(
+                                PropertyFactory.iconImage("marker-icon-id")
+                        );
+                        style.addLayer(symbolLayer);
+
+                    } // End of onStyleLoaded()
+                });
+            } // End of onMapReady()
+        }); // End of .getMapAsync()
+    } // End of onCreate()
+
+
+} // End of MainListingPage class
