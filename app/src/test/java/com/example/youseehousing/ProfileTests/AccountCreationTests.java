1c05c1bced85137fb9f2e1ebeae7156b1cf33679
aam0322 <ma.aaron.ma@gmail.com>
2019-05-25 16:19:16 -0700

    Added a curren tissue.

--- app/src/test/java/com/example/youseehousing/ProfileTests/AccountCreationTests.java
+++ app/src/test/java/com/example/youseehousing/ProfileTests/AccountCreationTests.java
@@ -6,9 +6,21 @@ package com.example.youseehousing.ProfileTests;
 public class AccountCreationTests {
 
 /**
+ * CURRENT ISSUES:
+ *     1. When signing up for a new account, clicking "next" brings me back to homepage even though
+ *        I haven't properly filled in the user account info.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
  * NOTE: Writing unit tests seems kind of difficult. So, I'll do it manually.
  *
+ * SIGN_UP_PAGE.JAVA:
  *
+ * Valid email address:
  *
  *
  *
