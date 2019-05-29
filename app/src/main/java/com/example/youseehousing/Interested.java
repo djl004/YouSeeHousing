051f4238c9821d991670b3ac1755e0e00c7508b7
mac <rainesmallwood@gmail.com>
2019-05-28 20:58:25 -0700

    I added interested class that has a setInterested method. This metho

---
+++ app/src/main/java/com/example/youseehousing/Interested.java
@@ -0,0 +1,47 @@
+package com.example.youseehousing;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+/**
+ *
+ *
+ * **/
+public class Interested {
+
+    private String TAG = "interested";
+
+    // listingId is address field
+    public void setInterested(final String userId, final String listingId) {
+        final FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        if (db != null) {
+            db.collection("listing")
+                    .whereEqualTo("address", listingId)
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if (task.isSuccessful()) {
+                                for (QueryDocumentSnapshot document : task.getResult()) {
+                                    // call function to get listing details
+                                    ArrayList<String> interested = (ArrayList<String>) document.get("interested");
+                                    interested.add(userId);
+                                    db.collection("listing").document(listingId).update("interested", interested);
+                                }
+                            } else {
+                                Log.w(TAG, "Error getting documents.", task.getException());
+                            }
+                        }
+                    });
+
+        }
+    }
+}
